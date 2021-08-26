15e389ad0f5bac76095e20a006f5959749d7a988
Joao Pedro <jp.ssilva@live.com>
2021-08-26 16:43:19 -0300

    Initial commit

---
+++ src/main/java/com/digital/crud/saladereuniao/saladereuniao/model/Room.java
@@ -0,0 +1,35 @@
+package com.digital.crud.saladereuniao.saladereuniao.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="meetingroom")
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "date", nullable = false)
+    private String date;
+
+    @Column(name = "startHour", nullable = false)
+    private String startHour;
+
+    @Column(name = "endHour", nullable = false)
+    private String endHour;
+
+
+}
